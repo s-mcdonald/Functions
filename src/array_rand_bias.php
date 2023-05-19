e7224003bd8e15ee4812f36c7199d6200e1279ba
sam <s.mcdonald@outlook.com.au>
2023-05-20 00:04:24 +1000

    DEV - Updated formula matrix, and fixed bug with returning value and not key in the match expression

--- src/array_rand_bias.php
+++ src/array_rand_bias.php
@@ -3,36 +3,26 @@
 if (!function_exists('array_rand_bias')) {
 
     /**
-     * @throws Exception
+     * @param array $array The `source` array to pluck a value from.
+     * @param string $default Will be returned when the array is provided empty.
+     * @return mixed The plucked value from the source array.
      */
-    function array_rand_bias(array &$array)
+    function array_rand_bias(array &$array, string $default = ""): mixed
     {
         if (empty($array)) {
-            throw new \Exception("Source array must have at least one value.");
+            return $default;
         }
 
-        $values = array_values($array);
         $m = (count($array)-1);
-        $score1 = mt_rand(0, 100);
-        $score2 = mt_rand(0, 100);
-        $score_total = ($score1 + $score2);
-
-        if (mt_rand(0, 100) < 30) {
-            return $values[0];
-        }
-
-        if (mt_rand(0, 100) < 40) {
-            return $values[
-                mt_rand(0, 1)
-            ];
-        }
+        $score_total = (mt_rand(0, 100) + mt_rand(0, 100));
 
         $index = match (true) {
-            $score_total > 90 => 0,
-            $score_total > 80 && $m < 3 => $values[mt_rand(0, 1)],
+            (mt_rand(0, 100) < 30) || $score_total > 120 => 0,
+            (mt_rand(0, 100) < 40) || $score_total > 100 && $m < 3 => mt_rand(0, 1),
+            (mt_rand(0, 100) < 50) || $score_total > 80 && $m < 4 => mt_rand(0, 2),
             default => mt_rand(0, $m),
         };
 
-        return $values[$index];
+        return array_values($array)[$index];
     }
 }
